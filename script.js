4ae76eb8bb31d8354c01674a30dcde1d6a855eff
KarenJosten <39259489+KarenJosten@users.noreply.github.com>
2020-06-21 13:00:22 +0200

    Script js Aufgabe 8

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-//Aufgabe 7.1
+//Aufgabe 8
 window.addEventListener("load", function () {
     document.querySelector("#A").addEventListener("click", function () { playSample(0); });
     document.querySelector("#C").addEventListener("click", function () { playSample(1); });
@@ -10,22 +10,87 @@ window.addEventListener("load", function () {
     document.querySelector("#laugh1").addEventListener("click", function () { playSample(6); });
     document.querySelector("#laugh2").addEventListener("click", function () { playSample(7); });
     document.querySelector("#snare").addEventListener("click", function () { playSample(8); });
-    //drumpad 7.2
-    document.querySelector("#play-button").addEventListener("click", drumPad);
+    //document.querySelector("#play-button").addEventListener("click", drumPad );
+    document.querySelector("#playbutton").addEventListener("click", playbtn);
+    document.querySelector("#trashbutton").addEventListener("click", deleteBeat);
+    document.querySelector("#microphone").addEventListener("click", record);
 });
 //Array mit allen Sounds
 var btn = ["sounds/A.mp3", "sounds/C.mp3", "sounds/F.mp3", "sounds/G.mp3", "sounds/hihat.mp3", "sounds/kick.mp3", "sounds/laugh-1.mp3", "sounds/laugh-2.mp3", "sounds/snare.mp3"];
+//Beat für Loop
+var loop = [4, 5, 8];
+//var snd: string [] = ["sounds/G.mp3", "sounds/hihat.mp3", "sounds/laugh-2.mp3"];
+var i = 0;
+//playbutton und stopbutton
+var interval = 0;
+function playbtn() {
+    // tslint:disable-next-line: typedef
+    let playstop = document.querySelector("#playbutton");
+    if (playstop.className == "fas fa-play") {
+        playstop.className = "fas fa-stop";
+        playBeat();
+    }
+    else {
+        playstop.className = "fas fa-play";
+        stopBeat();
+    }
+}
+//Stopbutton
+function stopBeat() {
+    clearInterval(interval);
+}
+//Loop
+function playBeat() {
+    // tslint:disable-next-line: typedef
+    interval = setInterval(function () {
+        playSample(loop[i]);
+        //+1
+        i++;
+        //Wenn i größer oder gleich als Länge von loop ist -> i = 0
+        if (i >= loop.length) {
+            i = 0;
+        }
+    }, 600);
+}
+//aktuellen Beat löschen
+function deleteBeat() {
+    //leeres Array
+    loop = [];
+}
+//neuen Beat aufnehmen
+var recordBeat = false;
+function record() {
+    if (recordBeat == false) {
+        recordBeat = true;
+    }
+    else {
+        recordBeat = false;
+    }
+}
 // tslint:disable-next-line: typedef
 function playSample(sounds) {
     var sound = new Audio(btn[sounds]);
     sound.play();
+    if (recordBeat == true) {
+        loop.push(sounds);
+    }
 }
-//Aufgabe 7.2
-function drumPad() {
-    setInterval(function () {
-        playSample(4);
-        playSample(5);
-        playSample(8);
-    }, 1000);
-}
+/*keydown
+window.addEventListener("load", function() {
+    document.querySelector("#A").addEventListener("keydown", function() {playSample(0); });
+    });
+
+var pad1: HTMLAudioElement = new Audio(btn[0]);
+// tslint:disable-next-line: typedef
+window.document.getElementById(function())
+var code = e.keydown;
+var kc = document.charset, string: (e.keydown);
+
+
+switch (kc) {
+        case "R":
+            pad1.load();
+            pad1.play();
+            break;
+    }*/
 //# sourceMappingURL=script.js.map
